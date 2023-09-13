e70abe93ee1f2b7beccd3d5dafb5a7f6fb6dce70
Shebeer <shebeeradly@gmail.com>
2023-09-13 12:30:37 +0530

    helmet

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import * as React from "react"
+import Helmet from "react-helmet";
 import type { HeadFC, PageProps } from "gatsby"
 import './style.css'
 import './style2.css'
@@ -146,6 +147,7 @@ const links = [
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <main style={pageStyles}>
+      <Helmet>
       <h1 className="title">
         Congratulations
         <br />
@@ -162,12 +164,13 @@ const IndexPage: React.FC<PageProps> = () => {
         src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
       />
      <MessageOutlined style={{ fontSize: '100px', color: '#08c' }} />;
+      </Helmet>
     </main>
   )
 }
 
 export default IndexPage
 
-// export const Head=()=>(<><title>The page title</title> 
-//  <meta name="description" content="Page Description" />
-//  <meta property="og:image" content={image} /></>)
+export const Head=()=>(<><title>The page title</title>
+ <meta name="description" content="Page Description" />
+ <meta property="og:image" content={image} /></>)
